05f3b493fbf1d98d8a21ac07e672a93978c50178
Alekssthlm <114854430+Alekssthlm@users.noreply.github.com>
2024-05-13 19:31:23 +0200

    Troubleshoot with metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   title: "Portfolio - Alexander Heras",
   description:
     "I'm passionate about web development and ready to embark on my professional journey. I'm currently enrolled in studies at Hyper Island, learning how to program by crafting web apps using industry best practices. I invite you to explore my work and let's connect!",
+  metadataBase: new URL("https://www.alexanderheras.com/"),
   openGraph: {
     title: "Portfolio - Alexander Heras",
     description:
